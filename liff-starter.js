f4f8853708aacd058b739dd08d3fcc40ed370b7c
Michael K <cahyadi.michael.mkc24@gmail.com>
2021-01-04 16:12:02 +0700

    first commit

---
+++ liff-starter.js
@@ -0,0 +1,127 @@
+var totalMakanan = 0;
+var totalMinuman = 0;
+var totalHarga = 0;
+
+$(document).ready(function(){
+    $(".jumlah").html("0");
+    $(".min").click(function(){
+        var i = $(this).siblings().closest(".jumlah").text();
+        if(i>0){
+            i--;
+            $(this).siblings().closest(".jumlah").text(i);
+            totalMakanan = parseInt($('.jumlahMakanan1').text()) + parseInt($('.jumlahMakanan2').text());
+            totalMinuman = parseInt($('.jumlahMinuman1').text()) + parseInt($('.jumlahMinuman2').text());
+            $('.totalJumlah').text('Total: '+totalMakanan+' makanan dan '+totalMinuman+' minuman');
+            totalHarga = parseInt($('.jumlahMakanan1').text())*parseInt($('.harga1').text()) + parseInt($('.jumlahMakanan2').text())*parseInt($('.harga2').text()) + parseInt($('.jumlahMinuman1').text())*parseInt($('.harga3').text()) + parseInt($('.jumlahMinuman2').text())*parseInt($('.harga4').text());
+            $('.totalHarga').text('Harga: '+totalHarga);
+        }
+    });
+    $(".add").click(function(){
+        var i = $(this).siblings().closest(".jumlah").text();
+        i++
+        $(this).siblings().closest(".jumlah").text(i);
+        totalMakanan = parseInt($('.jumlahMakanan1').text()) + parseInt($('.jumlahMakanan2').text());
+        totalMinuman = parseInt($('.jumlahMinuman1').text()) + parseInt($('.jumlahMinuman2').text());
+        $('.totalJumlah').text('Total: '+totalMakanan+' makanan dan '+totalMinuman+' minuman');
+        totalHarga = parseInt($('.jumlahMakanan1').text())*parseInt($('.harga1').text()) + parseInt($('.jumlahMakanan2').text())*parseInt($('.harga2').text()) + parseInt($('.jumlahMinuman1').text())*parseInt($('.harga3').text()) + parseInt($('.jumlahMinuman2').text())*parseInt($('.harga4').text());
+        $('.totalHarga').text('Harga: '+totalHarga);
+    });
+});
+
+window.onload = function() {
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "1655338109-RpZmDMGN";   // change the default LIFF value if you are not using a node server
+
+    // DO NOT CHANGE THIS
+    let myLiffId = "";
+
+    // if node is used, fetch the environment variable and pass it to the LIFF method
+    // otherwise, pass defaultLiffId
+    myLiffId = defaultLiffId;
+    initializeLiff(myLiffId);
+};
+
+/**
+* Initialize LIFF
+* @param {string} myLiffId The LIFF ID of the selected element
+*/
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            // start to use LIFF's api
+            initializeApp();
+        });
+}
+
+/**
+ * Initialize the app by calling functions handling individual app components
+ */
+function initializeApp() {
+    registerButtonHandlers();
+}
+
+/**
+* Register event handlers for the buttons displayed in the app
+*/
+function registerButtonHandlers() {
+
+    // get profile call
+    liff.getProfile().then(function(profile) {
+        document.getElementById('displayNameField').textContent = profile.displayName;
+
+        const profilePictureDiv = document.getElementById('profilePictureDiv');
+        if (profilePictureDiv.firstElementChild) {
+            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+        }
+        const img = document.createElement('img');
+        img.src = profile.pictureUrl;
+        img.alt = 'Profile Picture';
+        profilePictureDiv.appendChild(img);
+    });
+
+    // openWindow call
+    document.getElementById('openWindowButton').addEventListener('click', function() {
+        liff.openWindow({
+            url: 'https://cobapemesananmakanan.herokuapp.com/',
+            external: true
+        });
+    });
+
+    // sendMessages call
+    document.getElementById('sendMessageButton').addEventListener('click', function() {
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "Hi"+profile.displayName+",\n\nTerimakasih telah memesan makanan, berikut adalah review pesanannya:\n\n* "+totalMakanan+" Makanan\n* "+totalMinuman+" Minuman\n\nPesanan kakak akan segera diproses dan akan dibertahu jika sudah bisa diambil.\n\nMohon ditunggu ya!"
+        }]).then(function() {
+            window.alert('Message sent');
+        });
+    });
+
+    // login call, only when external browser is used
+    document.getElementById('liffLoginButton').addEventListener('click', function() {
+        if (!liff.isLoggedIn()) {
+            // set `redirectUri` to redirect the user to a URL other than the front page of your LIFF app.
+            liff.login();
+        }
+    });
+}
+
+function sendAlertIfNotInClient() {
+    alert('This button is unavailable as LIFF is currently being opened in an external browser.');
+}
+
+/**
+* Toggle specified element
+* @param {string} elementId The ID of the selected element
+*/
+function toggleElement(elementId) {
+    const elem = document.getElementById(elementId);
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = 'none';
+    } else {
+        elem.style.display = 'block';
+    }
+}
